4b83bd7e8ef24da1e1830cb810156e117b2addf7
zulfan14 <muhammadzulfan48@gmail.com>
2022-10-31 11:08:12 +0700

    useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Header from "./components/Header";
 function App() {
-  const title = "Welcome To React HOOKS";
-  const number = 12;
+  const [title, setTitle] = useState("Welcome To React HOOKS");
+  const [number, setNumber] = useState(12);
   const link = "https://github.com/zulfan14/";
 
-  const ClickMe = (name) => {
-    console.log("Cliked By " + name);
+  const Change = () => {
+    setTitle("Title Changed");
+    setNumber(10);
   };
+
+  // {
+  //   const ClickMe = (name) => {
+  //     console.log("Cliked By " + name);
+  //   };
+  // }
 
   return (
     <div>
@@ -14,7 +22,8 @@ function App() {
       <h1>{title}</h1>
       <h1>{number}</h1>
       <a href={link}>Go To my repo github</a>
-      <button onClick={() => ClickMe("Zulfan")}>Click Me</button>
+      {/* <button onClick={() => ClickMe("Zulfan")}>Click Me</button> */}
+      <button onClick={Change}>Change</button>
     </div>
   );
 }
