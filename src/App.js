8b38ba2ccc3274de0a4737a208ef6928c049c390
zulfan14 <muhammadzulfan48@gmail.com>
2022-10-31 14:53:03 +0700

    Looping List

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 import Header from "./components/Header";
 function App() {
   const [title, setTitle] = useState("Welcome To React HOOKS");
-  const [number, setNumber] = useState(12);
+  const [number, setNumber] = useState(10);
   const link = "https://github.com/zulfan14/";
+
+  const [product] = useState([
+    { id: 1, title: "product1", price: 899 },
+    { id: 2, title: "Product2", price: 879 },
+    { id: 3, title: "Produk2", price: 869 },
+  ]);
+
+  const [user] = useState([
+    { id: 1, name: "Andi", jk: "lk" },
+    { id: 2, name: "Budi", jk: "lk" },
+    { id: 3, name: "Ani", jk: "pr" },
+  ]);
 
   const Change = () => {
     setTitle("Title Changed");
-    setNumber(10);
+    setNumber(number + 1);
   };
 
   // {
@@ -21,7 +33,24 @@ function App() {
       <Header />
       <h1>{title}</h1>
       <h1>{number}</h1>
-      <a href={link}>Go To my repo github</a>
+      <a href={link} target="blank">
+        Go To my repo github
+      </a>
+      <ul>
+        {product.map((product) => (
+          <li key={product.id}>
+            {product.title} - {product.price}
+          </li>
+        ))}
+      </ul>
+      <h1>Ini adalah nama2 pengguna</h1>
+      <ul>
+        {user.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.jk}
+          </li>
+        ))}
+      </ul>
       {/* <button onClick={() => ClickMe("Zulfan")}>Click Me</button> */}
       <button onClick={Change}>Change</button>
     </div>
---
+++ src/Use_effect.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const UseEffect = () => {
+  const [color, setColor] = useState("black");
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
+  return (
+    <div>
+      {" "}
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
