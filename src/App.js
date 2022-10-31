0a90edfc1787e2d210d24990ad7cd601ee598692
zulfan14 <muhammadzulfan48@gmail.com>
2022-10-31 10:51:23 +0700

    Event Click

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ function App() {
   const title = "Welcome To React HOOKS";
   const number = 12;
   const link = "https://github.com/zulfan14/";
+
+  const ClickMe = (name) => {
+    console.log("Cliked By " + name);
+  };
+
   return (
     <div>
       <Header />
       <h1>{title}</h1>
       <h1>{number}</h1>
       <a href={link}>Go To my repo github</a>
+      <button onClick={() => ClickMe("Zulfan")}>Click Me</button>
     </div>
   );
 }
