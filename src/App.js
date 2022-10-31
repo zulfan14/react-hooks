ffbdcd1c4b69f8d42fa5abc35ecb17ccc365850a
zulfan14 <muhammadzulfan48@gmail.com>
2022-10-31 10:29:27 +0700

    Dynamic Value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "Welcome To React HOOKS";
+  const number = 12;
+  const link = "https://github.com/zulfan14/";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <h1>{number}</h1>
+      <a href={link}>Go To my repo github</a>
     </div>
   );
 }
