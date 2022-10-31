33bb3ce637399a3b95e1082a0e9cc90583e056e0
zulfan14 <muhammadzulfan48@gmail.com>
2022-10-31 10:42:06 +0700

    Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 function App() {
   const title = "Welcome To React HOOKS";
   const number = 12;
   const link = "https://github.com/zulfan14/";
   return (
     <div>
+      <Header />
       <h1>{title}</h1>
       <h1>{number}</h1>
       <a href={link}>Go To my repo github</a>
